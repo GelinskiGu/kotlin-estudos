99e2880159955ae9f7e968e1e12e6be07cf1a786
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-09 17:10:41 -0300

    Herança

---
+++ udemy/IntelliJ/POOHeranca/src/main/kotlin/Heranca.kt
@@ -0,0 +1,35 @@
+private class X private constructor() // Limita visibilidade no arquivo se private estiver antes da classe.
+// Se estiver no construtor, classe não pode ser instanciada
+
+// No protected, os filhos podem acessar os métodos/atributos.
+// No private, apenas a classe pode acessar os métodos/atributos.
+
+open class Electronic(private var marca: String) {
+    private fun ativarCorrente() {}
+    fun ligar(){
+        ativarCorrente()
+    }
+    fun desligar() {}
+}
+
+class Computer(marca: String) : Electronic(marca) { // Classe especializada herda da maior
+    fun instalarSoftware(){} // Comportamentos específicos
+    fun processar() {}
+
+}
+/*
+class Computer(var marca: String) {
+    fun ligar() {}
+    fun desligar() {}
+}
+*/
+
+
+fun main() {
+    val c = Computer("DELL")
+
+    // c.ativarCorrente() . Não faz sentido ativarCorrente e ligar depois. Modificador de acesso private
+    c.ligar()
+    c.desligar()
+    // c.marca . Está privado
+}
