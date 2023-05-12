0f6797029e95afdeb6fb61b9553dfb93bd5bc7c4
Gustavo Gelinski <gus_gelinski@hotmail.com>
2023-05-12 15:55:57 -0300

    Novo projeto: Motivation. Quase sendo finalizado.

---
+++ udemy/Motivation/app/src/main/java/com/example/motivation/ui/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.motivation.ui
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.motivation.infra.MotivationConstants
+import com.example.motivation.R
+import com.example.motivation.infra.SecurityPreferences
+import com.example.motivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        //supportActionBar?.hide()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        handleUserName()
+
+
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase) {
+            var s = ""
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun handleUserName() {
+        val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
+        binding.textUserName.text = "Olá, $name!"
+    }
+}
